b8265ca37bf796917d72bdf3e06521bec28c6f48
Alex Levashov <alex@magenable.com.au>
2019-08-10 12:26:16 +1000

    comments and clean up of the code

--- athena-test.R
+++ athena-test.R
@@ -4,16 +4,18 @@
 library(AWR.Athena)
 require(DBI)
 library(tidyverse)
-
+# You need AWS API user with proper access to S3 and Athena
 # AWS Access Key and Secret should be set via AWS CLI, run "aws configure" from command line
-        
+# S3OutputLocation should be taken from your Athena settings        
 con <- dbConnect(AWR.Athena::Athena(), region='us-west-2',
                  S3OutputLocation='s3://aws-athena-query-results-518190832416-us-west-2/',
                  Schema='default')
+# get list of tables available
 dbListTables(con)
+#query specific table (all records, SQL statement can be any supported by Athena)
 df <- as_tibble(dbGetQuery(con, "Select * from events"))
 
-# basic visualization, browsers, hits
+# basic visualization, hits by different browsers
 
 t<- df %>% group_by (br_family) %>% mutate (n=n())%>% 
         select (br_family, n) %>% count() %>% rename(browser=br_family, hits=n) %>% arrange (desc(hits))
@@ -25,4 +27,4 @@ t %>%   ggplot(aes(x="", y=hits, fill=browser))+
         geom_bar(width = 1, stat="identity")+
         coord_polar(theta = "y", start = 0)
 
-as.factor(t$browser)
+
